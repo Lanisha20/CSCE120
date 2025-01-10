26e3a41b9b2288be152f4dd237796d519a56661f
Joanna Ramesh <77648489+Lanisha20@users.noreply.github.com>
2025-01-09 23:09:09 -0600

    Add files via upload

---
+++ labworks/stack calculator/calculator.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+#include <string>
+#include <sstream>
+#include "functions.h"
+using std::cin, std::cout, std::endl, std::string, std::istringstream;
+
+int main() {
+  // prompt user input
+  cout << "Type RPN expression (end with '=')." << endl;
+  cout << "> ";
+  
+  //TODO: create a command-line interface for calculator GUI
+  /*Stack testStack;
+
+  push(testStack, 2);
+  push(testStack, 1);
+
+  //DEBUG(
+  peek(testStack);
+    
+  pop(testStack);
+
+  //DEBUG(
+  peek(testStack); */
+  return 0;
+}
